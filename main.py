b5ff8108d5a158d702f63c6d96218d3f7cb6bbca
cestarte <hakes.radius-0w@icloud.com>
2023-12-02 20:38:38 -0600

    improved settings

--- main.py
+++ main.py
@@ -3,24 +3,38 @@ from settings import *
 from prepare_database import *
 from wunderground import *
 from weather import *
+from dotenv import load_dotenv
+from json import JSONDecodeError
 
 
-def get_settings_or_die(config: Settings):
-    status = config.read("settings.json")
-    if status[0] is False:
-        print(f"FATAL: Unable to read settings.\n{str(status[1])}")
+def read_settings(full_path):
+    if os.path.exists(full_path) == False:
+        print(f'FATAL: Settings file does not exist at path "{full_path}"')
         os._exit(1)
-    else:
-        config.populate_values(status[1])
+    if os.access(full_path, os.R_OK) == False:
+        print(f'FATAL: Settings file is not accessible at path "{full_path}"')
+        os._exit(1)
+
+    try:
+        return Settings.read(full_path)
+    except JSONDecodeError as ex:
+        print("FATAL: Settings JSON failed to decode.")
+        if hasattr(ex, "message"):
+            print(str(ex.message))
+        os._exit(1)
+    except Exception as ex:
+        print(f'FATAL: Settings could not be read at path "{full_path}".')
+        print(ex)
+        os._exit(1)
 
 
 if __name__ == "__main__":
-    config = Settings()
-    get_settings_or_die(config)
+    load_dotenv()
+    config = read_settings(os.environ["SETTINGS_PATH"])
 
     # check for database accessibility or die
-    print(f"Checking for database at \"{config.values['database']}\"")
-    status = check_for_db(config.values["database"])
+    print(f'Checking for database at "{config.database}"')
+    status = check_for_db(config.database)
     if status[0] == False:
         print(
             f"FATAL: Unable to access database or file does not exist.\n{str(status[1])}"
@@ -28,7 +42,7 @@ if __name__ == "__main__":
         os._exit(1)
 
     weathers = []
-    for url in config.values["urls"]:
-        weathers.append(scrape(url))
+    # for url in config.urls:
+    #    weathers.append(scrape(url))
 
-    write_to_database(config.values["database"], weathers)
+    # write_to_database(config.database, weathers)
